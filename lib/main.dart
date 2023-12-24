6db9e881137773d07ac75fd08ce778fe2e3b32eb
sarahnosir <sarah.nosir@outlook.com>
2023-12-24 13:02:26 +0200

    Task12

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopify_app/firebase_options.dart';
-import 'package:shopify_app/models/product.model.dart';
 import 'package:shopify_app/pages/splash_page.dart';
 import 'package:shopify_app/provider/auth_provider.dart';
 import 'package:shopify_app/services/prefrences.service.dart';
 import 'package:shopify_app/utils/theme.utils.dart';
-
+import 'provider/product_provider.dart';
 import 'provider/home_provider.dart';
-import 'provider/productprovider.dart';
 
 void main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -19,13 +17,7 @@ void main() async {
 
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) => AppAuthProvider()),
-    FutureProvider<List<Product>?>(
-      create: (_) => ProductProvider().getProducts(),
-      initialData: null,
-      catchError: (_, err) {
-        return [];
-      },
-    ),
+    Provider(create: (_) => ProductProvider()),
     ChangeNotifierProvider(create: (_) => HomeProvider())
   ], child: MyApp()));
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,9 +4,11 @@ import 'package:shopify_app/provider/home_provider.dart';
 import 'package:shopify_app/widgets/home/carousel_slider_ex.widget.dart';
 import 'package:shopify_app/widgets/headline.widget.dart';
 import 'package:shopify_app/widgets/home/categories_row.home.widget.dart';
-import '../models/product.model.dart';
+import '../provider/category_provider.dart';
+import '../provider/product_provider.dart';
 import '../widgets/home/carouselwidget.dart';
 import '../widgets/home/prodouct_card.dart';
+import 'package:flexible_grid_view/flexible_grid_view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,7 +25,31 @@ class _HomePageState extends State<HomePage> {
         child: SingleChildScrollView(
             child: Column(children: [
           HeadlineWidget(title: 'Categories'),
-          CategoriesRowHome(),
+          Consumer<CategoryProvider>(
+            builder: (__, caegoryProvider, _) {
+              return FutureBuilder(
+                  future: caegoryProvider.getCategories(context, limit: 3),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('Error While Get Data');
+                      } else if (snapshot.hasData) {
+                        return CategoriesRowHome(
+                          categories: snapshot.data ?? [],
+                        );
+                      } else {
+                        return Text('No Data Found');
+                      }
+                    } else {
+                      return Text(
+                          'Connection Statue ${snapshot.connectionState}');
+                    }
+                  });
+            },
+          ),
           Consumer<HomeProvider>(
             builder: (context, advertisements, child) {
               if (advertisements == null) {
@@ -41,21 +67,35 @@ class _HomePageState extends State<HomePage> {
               }
             },
           ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Consumer<List<Product>?>(
-              builder: (ctx, productValues, _) {
-                if (productValues == null) {
-                  return CircularProgressIndicator();
-                } else if (productValues.isEmpty) {
-                  return Text('No Data Found');
-                } else {
-                  return Row(children: [
-                    ...productValues.map((prod) => ProductWidget(product: prod))
-                  ]);
-                }
-              },
-            ),
+          Consumer<ProductProvider>(
+            builder: (__, productProvider, _) {
+              return FutureBuilder(
+                  future: productProvider.getProducts(context, limit: 3),
+                  builder: (context, snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const CircularProgressIndicator();
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.done) {
+                      if (snapshot.hasError) {
+                        return Text('Error While Get Data');
+                      } else if (snapshot.hasData) {
+                        return FlexibleGridView(
+                          axisCount: GridLayoutEnum.threeElementsInRow,
+                          shrinkWrap: true,
+                          children: snapshot.data
+                                  ?.map((e) => ProductWidget(product: e))
+                                  .toList() ??
+                              [],
+                        );
+                      } else {
+                        return Text('No Data Found');
+                      }
+                    } else {
+                      return Text(
+                          'Connection Statue ${snapshot.connectionState}');
+                    }
+                  });
+            },
           ),
         ])));
   }
